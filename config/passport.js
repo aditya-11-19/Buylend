784ffc834cd0c4ac5e43db1f62a5d1243f2f5b01
VAIBHAV GARG <63347618+Vaibhav2801@users.noreply.github.com>
2022-12-22 18:57:11 +0530

    Backend Started

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const JWTStrategy=require('passport-jwt').Strategy
+const ExtractJWT=require('passport-jwt').ExtractJwt
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+
+module.exports = passport => {
+    passport.use(
+      new JwtStrategy(opts, (jwt_payload, done) => {
+        // User.findById(jwt_payload.id)
+        //   .then(user => {
+        //     if (user) {
+        //       return done(null, user);
+        //     }
+        //     return done(null, false);
+        //   })
+        //   .catch(err => console.log(err));
+      })
+    );
+  };
+  
---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+
+const app = express();
+
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  // Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
